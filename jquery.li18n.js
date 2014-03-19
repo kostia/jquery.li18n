025876d01242a5360d18af3c9e1572424441d7f4
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-03-19 19:29:22 +0100

    v0.1.2

--- jquery.li18n.js
+++ jquery.li18n.js
@@ -38,7 +38,7 @@
   };
 
   $.li18n = {
-    version: '0.1.1',
+    version: '0.1.2',
 
     _translate: function(key, interpolationOptions) {
       assertPresent(key, 'Tried to translate with an empty key');
